6def7a9220ea9c5afe17bd1f4db85713cfe165d4
sinthyasithy <145050483+sinthyasithy@users.noreply.github.com>
2024-04-29 21:10:51 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let testdata = {
+    "user": {
+      "name": "John Doe",
+      "age": 30,
+      "email": "john.doe@example.com"
+    },
+    "products": [
+      {
+        "name": "Product A",
+        "price": 19.99,
+        "quantity": 2
+      },
+      {
+        "name": "Product B",
+        "price": 29.99,
+        "quantity": 1
+      }
+    ],
+    "total": 69.97
+  }
+  
+  console.log(testdata);
+console.log(testdata.user);
+console.log(testdata.user.name);
+console.log(testdata.user.age);
+console.log(testdata.user.email);
+console.log(testdata.products);
